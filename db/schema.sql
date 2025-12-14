2b7dc1feb56162d05cb60c328284c4ee491f8f7b
Zachary <zbergman2701@gmail.com>
2025-12-14 11:06:31 -0500

    created the table in scheme

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
 -- TODO
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text UNIQUE NOT NULL,
+  password text NOT NULL
+);
+
+CREATE TABLE orders (
+  id serial PRIMARY KEY,
+  date date NOT NULL,
+  note text,
+  user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE orders_products (
+orders_id int PRIMARY KEY,
+product_id int PRIMARY KEY,
+quantity int NOT NULL
+);
+
+CREATE TABLE products (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  description text NOT NULL,
+  price decimal NOT NULL
+);
