264bcc31abaea8799da43708e6f3150d9fc7c1ef
Zachary <zbergman2701@gmail.com>
2025-12-14 10:26:53 -0500

    Filled out some error conditions

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 import express from "express";
 const app = express();
 export default app;
+
+import usersRouter from "#api/users";
+import getUserFromToken from "#middleware/getUserFromToken";
+
+app.use(express.json());
+
+app.use(getUserFromToken);
+
+app.use("/tasks", tasksRouter);
+app.use("/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  switch (err.code) {
+    case "22P02":
+      return res.status(400).send(err.message);
+    case "23505":
+    case "23503":
+      return res.status(400).send(err.detail);
+    default:
+      next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong.");
+});
