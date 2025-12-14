d2a14107a01e6e25e9a6d7cf075560e109c0ae7d
Zachary <zbergman2701@gmail.com>
2025-12-14 11:08:44 -0500

    database seeded

--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,8 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  const user = await createUser("wobble", "thud");
+  for (let i = 1; i <= 5; i++) {
+    await createTask(`Task ${i}`, false, user.id);
+  }
 }
