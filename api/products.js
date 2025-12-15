bdb61b9e66a8b7c5f75b6316ccbd6334d5c9ca42
Zachary <zbergman2701@gmail.com>
2025-12-14 22:14:58 -0500

    added in quiery users and commented back in products

--- api/products.js
+++ api/products.js
@@ -14,20 +14,19 @@ router.get(
 
 router.get("products/:id", async (req, res) => {
   const { id } = req.params;
-  /* Need to determine conditional statement 
+  /* Need to determine conditional statement*/
   const product = products.find((p) => p.id === Number(id));
-    if (!product) {
-        return res.status(404).send("Invalid product id.");
-    }
-    */
+  if (!product) {
+    return res.status(404).send("Invalid product id.");
+  }
+
   res.send(product);
 });
 
 router.get("products/:id/orders", authenticate, (req, res) => {
-  //const {id} = req, params;
+  const { id } = req.params;
 
-  //const product = products.find(p => p.id === Number(id));
-
+  const product = products.find((p) => p.id === Number(id));
   /* Need to determine conditional statement */
   if (!product) return res.status(404).send("Invalid product id.");
   if (!authenticate) return res.status(401).send("Unauthorized Error");
